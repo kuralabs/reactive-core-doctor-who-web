33c11edf593046e5eef9b553a07098a777388b29
Carolina Aguilar <caroagse@gmail.com>
2017-02-24 05:00:53 -0600

    Added doctor detail modal and styles.

---
+++ src/components/Doctor.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import './styles/doctor.css'
+
+const DoctorRow = ({ closeModal, doctor }) => (
+    <div>
+        <div className="button-wrapper">
+            <button onClick={closeModal} className="button-close">
+                X
+            </button>
+        </div>
+
+        <div className="header-wrapper">
+            <span className="header">#{doctor.doctorNumber}</span>
+            <img className="header-img"src={doctor.image} alt={doctor.actor}/>
+        </div>
+
+
+        <p className="text">{doctor.description}</p>
+        <p className="title">Who Played Him:</p>
+        <p className="text">{doctor.actor}</p>
+        <p className="title">When:</p>
+        <p className="text">{doctor.when}</p>
+        <p className="title">Memorable Gadget/Item of Clothing:</p>
+        <p className="text">{doctor.gadget}</p>
+        <p className="title">Reason for Regeneration:</p>
+        <p className="text">{doctor.regeneration}</p>
+        <p className="title">Three Recommended Stories:</p>
+        <p className="text">{doctor.stories}</p>
+    </div>
+)
+
+export default DoctorRow
--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Modal from 'react-modal'
 
+import Doctor from './Doctor'
 import DoctorRow from './DoctorRow'
 
 import './styles/doctors.css';
+import './styles/doctor.css';
 
 const Doctors = ({ doctors, showDoctor, doctor, orderByName, orderByNumber, selectDoctor, showDoctorslist }) => (
     <div className="container">
@@ -15,6 +18,16 @@ const Doctors = ({ doctors, showDoctor, doctor, orderByName, orderByNumber, sele
                 />
             )}
         </ul>
+        <Modal isOpen={showDoctor}
+            overlayClassName="modal-overlay"
+            className="modal"
+            contentLabel="Doctor"
+        >
+            <Doctor
+                doctor={doctor}
+                closeModal={showDoctorslist}
+            />
+        </Modal>
     </div>
 )
 
